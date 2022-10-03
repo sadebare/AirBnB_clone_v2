1a00124a1d16fad00103db3343ed2059e14af18e
sadebare <adebaresulaimon04@gmail.com>
2022-10-03 10:22:13 +0100

    create a tar file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+generate a .tgz achive from the contents of web_static website
+usage:
+  fab -f 1-pack_web_static.py do_pack
+"""
+from fabric.api import local
+from datetime import datetime
+
+def do_pack():
+    """Generates .tgz archive from the contents of /web_static
+       returns archive's path if successful and None if not
+    """
+    now = datetime.now().strftime('%Y%m%d%H%M%S')
+    filepath = 'versions/web_static_{}.tgz'.format(now)
+
+    local('mkdir -p versions/')
+    createArchive = local('tar -cvzf {} web_static/'.format(filepath))
+
+    if createArchive.succeeded:
+        return filepath
